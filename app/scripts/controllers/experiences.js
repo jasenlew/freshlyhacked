d3059fb315f6d3aac41c92af5fc4a9bebd01cab3
Jasen Lew <jasen.lew@gmail.com>
2014-07-07 12:10:18 -0700

    Ability to add experience

--- app/scripts/controllers/experiences.js
+++ app/scripts/controllers/experiences.js
@@ -1,5 +1,28 @@
 'use strict';
 
 app.controller('ExperiencesController', function ($scope) {
+  // Excluding Notes, Tags, Pics and Address
+  $scope.experiences = [];
+
+  $scope.experience = {
+      name: '',
+      url: 'http://www.',
+      lat: '',
+      lon: '',
+      duration: '',
+      price: ''
+  };
+
+  $scope.submitExperience = function () {
+    $scope.experiences.push($scope.experience);
+    $scope.experience = {
+      name: '',
+      url: 'http://www.',
+      lat: '',
+      lon: '',
+      duration: '',
+      price: ''
+    };
+  };
 
 });
